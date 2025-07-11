cde1c61d293a7db56aa22149a1471bc1c1a63f74
Fast4x <rinorusso@gmail.com>
2025-07-11 23:36:40 +0200

    Code refactoring

--- composeApp/src/androidMain/kotlin/it/fast4x/riplay/extensions/chromecast/CastUtils.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/riplay/extensions/chromecast/CastUtils.kt
@@ -1,26 +1,12 @@
 package it.fast4x.riplay.extensions.chromecast
 
-import android.app.Activity
-import android.content.Context
-import android.os.Handler
-import android.os.Looper
-import android.view.MenuItem
-import androidx.compose.ui.graphics.Color
 import com.google.android.gms.cast.framework.CastContext
-import com.google.android.gms.cast.framework.CastState
-import com.google.android.gms.cast.framework.CastStateListener
-import com.google.android.gms.cast.framework.IntroductoryOverlay
-import com.google.android.gms.cast.framework.SessionManager
-import com.google.android.gms.tasks.Task
 import com.pierfrancescosoffritti.androidyoutubeplayer.chromecast.chromecastsender.ChromecastYouTubePlayerContext
 import com.pierfrancescosoffritti.androidyoutubeplayer.chromecast.chromecastsender.io.infrastructure.ChromecastConnectionListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import it.fast4x.riplay.appContext
-import it.fast4x.riplay.context
-import it.fast4x.riplay.typography
-import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 import java.util.concurrent.Executors
 
@@ -28,25 +14,6 @@ import java.util.concurrent.Executors
 fun initChromecast() {
     println("initChromecast")
     val castExecutor = Executors.newSingleThreadExecutor()
-//    val castContext = CastContext.getSharedInstance(appContext(), castExecutor).result
-//    val sessionManager = castContext.sessionManager
-//    ChromecastYouTubePlayerContext(
-//        sessionManager,
-//        SimpleChromecastConnectionListener()
-//    )
-
-    //val castContext = getCastContext(appContext())
-//    val sessionManager = castContext.sessionManager
-//    ChromecastYouTubePlayerContext(
-//        sessionManager,
-//        SimpleChromecastConnectionListener()
-//    )
-
-//   val mCastStateListener = CastStateListener { newState ->
-//        if (newState != CastState.NO_DEVICES_AVAILABLE) {
-//            showIntroductoryOverlay()
-//        }
-//    }
     var castContext: CastContext? = null
     CastContext.getSharedInstance(appContext(), castExecutor).addOnCompleteListener {
         println("initChromecast addOnCompleteListener CastContext.getSharedInstance")
