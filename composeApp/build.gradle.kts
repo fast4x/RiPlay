d89148f6384bcac22d80133267f7347f75d2bb8c
Fast4x <rinorusso@gmail.com>
2025-09-14 11:21:32 +0200

    Code refactoring

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -136,7 +136,7 @@ android {
         applicationId = "it.fast4x.riplay"
         minSdk = 21
         targetSdk = 36
-        versionCode = 17
+        versionCode = 18
         versionName = "0.0.$versionCode-alpha"
 
         multiDexEnabled = true
