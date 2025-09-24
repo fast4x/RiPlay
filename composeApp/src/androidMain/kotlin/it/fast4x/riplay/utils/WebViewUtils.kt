5596a0fac0c21eb4ae6d965affef4b7b3ae3a26f
Fast4x <rinorusso@gmail.com>
2025-09-24 22:26:14 +0200

    Code refactoring

--- composeApp/src/androidMain/kotlin/it/fast4x/riplay/utils/WebViewUtils.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/riplay/utils/WebViewUtils.kt
@@ -2,15 +2,22 @@ package it.fast4x.riplay.utils
 
 import android.webkit.CookieManager
 import android.webkit.WebStorage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 fun clearWebViewData(){
+    Timber.e("OnlinePlayerCore: clearWebkitData called")
     // Try delete all data cache and cookies
-    runCatching {
-        WebStorage.getInstance().deleteAllData()
-        CookieManager.getInstance().removeAllCookies(null)
-        CookieManager.getInstance().flush()
-    }.onFailure {
-        Timber.e("OnlinePlayerCore: onError clearWebkitData failed: ${it.message}")
+    CoroutineScope(Dispatchers.IO).launch {
+        runCatching {
+            WebStorage.getInstance().deleteAllData()
+            CookieManager.getInstance().removeAllCookies(null)
+            CookieManager.getInstance().flush()
+        }.onFailure {
+            Timber.e("OnlinePlayerCore: onError clearWebkitData failed: ${it.message}")
+        }
     }
+
 }
