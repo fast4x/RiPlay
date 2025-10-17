8e06bb65e026435cf91b4f621cdf07334c36f53b
Fast4x <rinorusso@gmail.com>
2025-10-17 23:50:25 +0200

    New alpha version 0.0.36

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -137,7 +137,7 @@ android {
         applicationId = "it.fast4x.riplay"
         minSdk = 21
         targetSdk = 36
-        versionCode = 35
+        versionCode = 36
         versionName = "0.0.$versionCode-alpha"
 
         multiDexEnabled = true
