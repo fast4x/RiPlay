9c3a5d287b0672140d4e965a5160126bd06568fe
fast4x <rinorusso@gmail.com>
2025-12-15 23:08:14 +0100

    Improvements and fix

--- composeApp/src/androidMain/kotlin/it/fast4x/riplay/extensions/rewind/utils/Utils.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/riplay/extensions/rewind/utils/Utils.kt
@@ -3,6 +3,7 @@ package it.fast4x.riplay.extensions.rewind.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import com.mikepenz.hypnoticcanvas.shaders.BlackCherryCosmos
 import com.mikepenz.hypnoticcanvas.shaders.GoldenMagma
 import com.mikepenz.hypnoticcanvas.shaders.GradientFlow
@@ -13,6 +14,7 @@ import com.mikepenz.hypnoticcanvas.shaders.PurpleLiquid
 import it.fast4x.riplay.data.Database
 import kotlinx.coroutines.Dispatchers
 import java.util.Calendar
+import kotlin.collections.listOf
 
 fun getRewindYear(): Int {
     val currentYear = Calendar.getInstance().get(Calendar.YEAR)
@@ -44,4 +46,14 @@ fun shadersList() = listOf(
     PurpleLiquid,
     InkFlow,
     OilFlow
+)
+
+fun colorsList() = listOf(
+    listOf(Color(0xFFE91E63), Color(0xFF9C27B0), Color(0xFF3F51B5)),
+    listOf(Color(0xFFE7D858), Color(0xFF733B81)),
+    listOf(Color(0xFF5A6CD2), Color(0xFF1DB954)),
+    listOf(Color(0xFF2196F3), Color(0xFF3F51B5)),
+    listOf(Color(0xFFFF9800), Color(0xFFFF5722)),
+    listOf(Color(0xFFE91E63), Color(0xFF9C27B0)),
+    listOf(Color(0xFF1DB954), Color(0xFFBBA0A0)),
 )
