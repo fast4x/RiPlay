1b4505fb5b73c493e5f8da4fea19ca10f5a654c8
fast4x <rinorusso@gmail.com>
2026-01-15 22:27:42 +0100

    Improvements and fix

--- composeApp/src/androidMain/kotlin/it/fast4x/riplay/utils/ServiceUtils.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/riplay/utils/ServiceUtils.kt
@@ -1,5 +1,6 @@
 package it.fast4x.riplay.utils
 
+import android.content.Intent
 import androidx.annotation.OptIn
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
@@ -12,14 +13,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startForegroundService
 import androidx.media3.common.util.UnstableApi
 import it.fast4x.riplay.LocalPlayerServiceBinder
 import it.fast4x.riplay.R
 import it.fast4x.riplay.extensions.preferences.rememberPreference
 import it.fast4x.riplay.extensions.preferences.restartActivityKey
+import it.fast4x.riplay.service.PlayerService
 import it.fast4x.riplay.ui.components.themed.SecondaryTextButton
 import it.fast4x.riplay.ui.components.themed.SmartMessage
 import it.fast4x.riplay.ui.screens.settings.SettingsDescription
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(UnstableApi::class)
 @Composable
@@ -28,7 +35,6 @@ fun RestartPlayerService(
     onRestart: () -> Unit
 ) {
     val context = LocalContext.current
-    val binder = LocalPlayerServiceBinder.current
     AnimatedVisibility(visible = restartService) {
         Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
             SettingsDescription(
@@ -39,7 +45,17 @@ fun RestartPlayerService(
             SecondaryTextButton(
                 text = stringResource(R.string.restart_service),
                 onClick = {
-                    binder?.restartForegroundOrStop().let { onRestart() }
+                    val intent = Intent(context, PlayerService::class.java)
+                    context.stopService(intent)
+                    if (isAtLeastAndroid8)
+                        context.startForegroundService(intent)
+                    else
+                        context.startService(intent)
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        delay(1000)
+                    }.invokeOnCompletion { onRestart() }
+
                     SmartMessage(context.resources.getString(R.string.done), context = context )
                 },
                 modifier = Modifier
