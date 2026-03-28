0e753e5a81ae09f7ef6ecc4ba1b543eb744606bc
fast4x <rinorusso@gmail.com>
2026-03-29 00:03:39 +0100

    Add empty state and GitHub link to RiTuneSelector

--- composeApp/src/androidMain/kotlin/it/fast4x/riplay/extensions/ritune/RiTuneSelector.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/riplay/extensions/ritune/RiTuneSelector.kt
@@ -20,17 +20,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import it.fast4x.riplay.R
 import it.fast4x.riplay.ui.components.themed.IconButton
 import it.fast4x.riplay.ui.components.themed.TitleSection
+import it.fast4x.riplay.ui.styling.bold
 import it.fast4x.riplay.utils.GlobalSharedData
 import it.fast4x.riplay.utils.colorPalette
+import it.fast4x.riplay.utils.typography
 
 @Composable
 fun RiTuneSelector(
     onDismiss: () -> Unit,
 ) {
+    val uriHandler = LocalUriHandler.current
     val riTuneDevices = GlobalSharedData.riTuneDevices.value.distinctBy { it.host }.distinctBy { it.port }
     Box(
         modifier = Modifier
@@ -38,61 +43,84 @@ fun RiTuneSelector(
             .fillMaxHeight(.5f)
     ) {
 
-        LazyColumn(
-            state = rememberLazyListState(),
-            contentPadding = PaddingValues(all = 10.dp),
-            modifier = Modifier
-                .background(colorPalette().background0)
-        ) {
-            item {
-                TitleSection("RiTune Cast")
-                Text(
-                    text = "Available devices:",
-                    color = colorPalette().text,
-                    modifier = Modifier.padding(bottom = 10.dp)
-                )
-            }
-
-            items(
-                count = riTuneDevices.size,
-                key = { index -> riTuneDevices[index].name }
-            ) { index ->
-
-                val device = riTuneDevices[index]
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(36.dp)
-                        .clickable {
-                            val devices = riTuneDevices.toMutableList()
-
-                            val updatedDevice = device.copy(selected = !device.selected)
-
-                            devices[index] = updatedDevice
-
-                            GlobalSharedData.riTuneDevices.value = devices.toMutableStateList()
-
-                            onDismiss()
-                        },
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    IconButton(
-                        icon = if (device.selected) R.drawable.cast_connected else R.drawable.cast_disconnected,
-                        color = colorPalette().text,
-                        enabled = true,
-                        onClick = {},
-                        modifier = Modifier.size(22.dp),
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
+            LazyColumn(
+                state = rememberLazyListState(),
+                contentPadding = PaddingValues(all = 10.dp),
+                modifier = Modifier
+                    .background(colorPalette().background0)
+            ) {
+                item {
+                    TitleSection("RiTune Cast")
                     Text(
-                        text = device.name,
+                        text = "Available devices:",
                         color = colorPalette().text,
-                        //modifier = Modifier.border(BorderStroke(1.dp, Color.Red))
+                        modifier = Modifier.padding(bottom = 10.dp)
                     )
                 }
+
+                if (riTuneDevices.isEmpty())
+                    item {
+                        Text(
+                            text = stringResource(R.string.no_devices_found_install_ritune_on_tv_or_other_device_to_use_this_feature),
+                            color = colorPalette().text,
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .padding(horizontal = 30.dp)
+                                .padding(bottom = 10.dp)
+                                .padding(top = 30.dp)
+                        )
+                        Text(
+                            text = "https://github.com/Fast4x/RiTune",
+                            color = colorPalette().text,
+                            style = typography().xs.bold,
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .padding(horizontal = 30.dp)
+                                .clickable { uriHandler.openUri("https://github.com/Fast4x/RiTune") }
+                        )
+                    }
+                else
+                    items(
+                        count = riTuneDevices.size,
+                        key = { index -> riTuneDevices[index].name }
+                    ) { index ->
+
+                        val device = riTuneDevices[index]
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(36.dp)
+                                .clickable {
+                                    val devices = riTuneDevices.toMutableList()
+
+                                    val updatedDevice = device.copy(selected = !device.selected)
+
+                                    devices[index] = updatedDevice
+
+                                    GlobalSharedData.riTuneDevices.value =
+                                        devices.toMutableStateList()
+
+                                    onDismiss()
+                                },
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            IconButton(
+                                icon = if (device.selected) R.drawable.cast_connected else R.drawable.cast_disconnected,
+                                color = colorPalette().text,
+                                enabled = true,
+                                onClick = {},
+                                modifier = Modifier.size(22.dp),
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Text(
+                                text = device.name,
+                                color = colorPalette().text,
+                                //modifier = Modifier.border(BorderStroke(1.dp, Color.Red))
+                            )
+                        }
+                    }
             }
-        }
 
         LinearProgressIndicator(
             modifier = Modifier
