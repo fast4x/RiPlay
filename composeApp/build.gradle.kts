4ff5db6d3b67437fea4b530110e8e6da78ae0ede
fast4x <rinorusso@gmail.com>
2026-04-24 22:47:37 +0200

    New release version 0.7.76

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -157,8 +157,8 @@ android {
         applicationId = "it.fast4x.riplay"
         minSdk = 24
         targetSdk = 36
-        versionCode = 75
-        versionName = "0.7.75"
+        versionCode = 76
+        versionName = "0.7.76"
 
         multiDexEnabled = true
 
--- createtag.cmd
+++ createtag.cmd
@@ -1,4 +1,4 @@
 git fetch --tags
-git tag v0.7.75
-git push origin v0.7.75
-git log v0.7.74..v0.7.75 --oneline
+git tag v0.7.76
+git push origin v0.7.76
+git log v0.7.75..v0.7.76 --oneline
