f42ee88c21cfe5a1ece0a514100d00bfc336a031
fast4x <rinorusso@gmail.com>
2026-05-29 18:37:07 +0200

    Prepare new release

--- createtag.cmd
+++ createtag.cmd
@@ -1,4 +1,4 @@
 git fetch --tags
-git tag v0.7.80
-git push origin v0.7.80
-git log v0.7.79..v0.7.80 --oneline
+git tag v0.7.81
+git push origin v0.7.81
+git log v0.7.80..v0.7.81 --oneline
--- deletetag.cmd
+++ deletetag.cmd
@@ -1,1 +1,1 @@
-git tag -d v0.7.80
+git tag -d v0.7.81
