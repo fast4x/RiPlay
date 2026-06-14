3f6d270cbdcde1bfb3b5be4a2c9446206f478d1f
fast4x <rinorusso@gmail.com>
2026-06-14 17:42:05 +0200

    Updated tag

--- createtag.cmd
+++ createtag.cmd
@@ -1,4 +1,4 @@
 git fetch --tags
-git tag v0.7.83
-git push origin v0.7.83
-git log v0.7.82..v0.7.83 --oneline
+git tag v0.7.84
+git push origin v0.7.84
+git log v0.7.83..v0.7.84 --oneline
--- deletetag.cmd
+++ deletetag.cmd
@@ -1,1 +1,1 @@
-git tag -d v0.7.83
+git tag -d v0.7.84
