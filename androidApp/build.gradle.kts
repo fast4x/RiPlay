bfaf12ebe44b389f80350d8391bc91f1c6dad8f2
fast4x <rinorusso@gmail.com>
2026-08-11 22:09:59 +0200

    Update version to 0.7.87 (88)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -161,8 +161,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "it.fast4x.riplay"
         targetSdk = 37
 
-        versionCode = 87
-        versionName = "0.7.86"
+        versionCode = 88
+        versionName = "0.7.87"
 
 
         // INIT ENVIRONMENT
--- createtag.cmd
+++ createtag.cmd
@@ -1,4 +1,4 @@
 git fetch --tags
-git tag v0.7.86
-git push origin v0.7.86
-git log v0.7.85..v0.7.86 --oneline
+git tag v0.7.87
+git push origin v0.7.87
+git log v0.7.86..v0.7.87 --oneline
